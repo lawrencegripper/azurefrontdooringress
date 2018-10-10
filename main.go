3ee90819846c78d903f99bd1a80420e272fa1290
Lawrence Gripper <info@grippers.co.uk>
2018-10-09 17:35:40 -0700

    Working basic connection to Frontdoor

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	// "github.com/Azure/azure-sdk-for-go/services/preview/frontdoor/mgmt/2018-08-01-preview/frontdoor"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/services/preview/frontdoor/mgmt/2018-08-01-preview/frontdoor"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
+	v1beta1 "k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	// "k8s.io/client-go/tools/cache"
-	v1beta1 "k8s.io/api/extensions/v1beta1"
 	"k8s.io/client-go/tools/clientcmd"
-
-	"time"
-
-	"os"
-	"path/filepath"
-	// batchv1 "k8s.io/api/batch/v1"
-	// apiv1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -25,6 +25,15 @@ const (
 )
 
 func main() {
+	ctx := context.Background()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Error("Error loading .env file")
+	}
+
+	subID := os.Getenv("AZURE_SUBSCRIPTION_ID")
+
 	resyncPeriod := 30 * time.Second
 	client, _ := getClientSet()
 	// create informers factory, enable and assign required informers
@@ -41,16 +50,30 @@ func main() {
 	time.Sleep(15 * time.Second)
 
 	log.Info("Resyncing data store")
-	err := store.Resync()
+	err = store.Resync()
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(store.List())
 
+	fdBackendClient := frontdoor.NewBackendPoolsClient(subID)
+	// create an authorizer from env vars or Azure Managed Service Idenity
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err == nil {
+		fdBackendClient.Authorizer = authorizer
+	}
+
 	for _, ingressObj := range store.List() {
 		ingress := ingressObj.(*v1beta1.Ingress)
-
+		result, err := fdBackendClient.CheckFrontDoorNameAvailability(ctx, frontdoor.CheckNameAvailabilityInput{
+			Name: to.StringPtr("testfdname"),
+			Type: frontdoor.MicrosoftNetworkfrontDoors,
+		})
+		if err != nil {
+			log.WithError(err).Fatal("Failed to check fd name")
+		}
+		fmt.Println(result)
 		fmt.Println(ingress.GetName())
 	}
 
