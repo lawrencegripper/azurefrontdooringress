2fdce6f0e8996db903e80af98d298eb9fcf1d74e
Lawrence Gripper <info@grippers.co.uk>
2018-10-16 18:28:30 +0100

    Attempting to make minikube work in travis #14

--- scripts/startminikube.sh
+++ scripts/startminikube.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-set -e
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo cp kubectl /usr/local/bin/ && rm kubectl
@@ -12,9 +11,14 @@ mkdir -p $HOME/.kube
 mkdir -p $HOME/.minikube
 touch $HOME/.kube/config
 
+echo "--> Starting minikube"
 export KUBECONFIG=$HOME/.kube/config
-sudo -E minikube start --vm-driver=none
+sudo -E minikube start --vm-driver=none --kubernetes-version=v1.8.0
 
+echo "--> Updating context"
+minikube update-context
+
+echo "--> Waiting for cluster to be usable"
 # this for loop waits until kubectl can access the api server that Minikube has created
 for i in {1..150}; do # timeout for 5 minutes
    kubectl get po &> /dev/null
